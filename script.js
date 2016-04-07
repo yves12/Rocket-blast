a9142f8a945e9eb380c06ea7a92f3a127905511c
yves1 <yvesoubida@yahoo.com>
2016-04-07 22:23:09 +0000

    added sounded and landing animation

---
+++ script.js
@@ -0,0 +1,85 @@
+var levels = [
+  {
+    wrong:'$("# rocketship") .s lideD o wn (3000) ;',
+    right:'$("#rocketship").slideDown(3000);',
+    message:"YOU NEED A SHIP TO ESCAPE, BUILD IT!",
+    code: function(){
+     $("#rocketship").slideDown(3000);
+    }
+  },
+  {
+    wrong:'$ ("#window").f adeIn(3000);',
+    right:'$("#window").fadeIn(3000);',
+    message:"ADD A WINDOW, YOU DON'T WANT TO MISS THE VIEW!",
+    code: function(){
+     $("#window").fadeIn(3000);
+    }
+  },
+  {
+    wrong:'$ (" . detail -colo r" ). css("border -bottom-color"," orange");',
+    right:'$(".detail-color").css("border-bottom-color","orange");',
+    message:"DON'T BE BORING, ADD COLORS.",
+    code: function(){
+     $(".detail-color").css("border-bottom-color","orange");
+    }
+  },
+  {
+    wrong:'$ ("#thruster"). fade In() ;',
+    right:'$("#thruster").fadeIn();',
+    message:"HURRY, START THE ENGINE!",
+    code: function(){
+     $("#thruster").fadeIn(); 
+    }
+  },
+  {
+ wrong:'$ ("#r ock etship") .animate ( {top :" -700px"}, 4000);',
+ right:'$("#rocketship").animate({top:"-700px"},4000);',
+    message:"BLAST OFF!",
+    code: function(){
+         
+    }
+}];
+
+var level = 0;
+
+function setlevel(level){
+
+  $('#box').val(levels[level].wrong);
+  $('#message').text(levels[level].message);
+}
+
+setlevel(level);
+
+$("#button").click(function(){
+
+  var ans =  $('#box').val(); 
+  
+  if(ans !== levels[level].right){
+    document.getElementById("Wrong").play(); 
+  }
+  if(ans === levels[level].right){
+    
+    if(ans === levels[4].right){
+    
+     $("#rocketship").animate({top:"-700px"},4000);
+    $("#rocketship").animate({top:"0px"},4000);
+      document.getElementById("rocket").play();
+      $("body").css("background", "url('https://upload.wikimedia.org/wikipedia/commons/5/58/Mars_23_aug_2003_hubble.jpg')");
+
+    }else{
+    
+    levels[level].code();
+    document.getElementById("Airwrench").play();
+    level++;
+    setlevel(level); 
+  }
+ }
+});
+ 
+$("#rocketship").hide();
+$("#window").hide();
+$("#thruster").hide();
+
+
+
+
